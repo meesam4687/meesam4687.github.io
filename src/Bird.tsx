23e2bd0d08b42ada0276b23f1dc88a43c6ed5a0d
meesam4687 <meesam4687@gmail.com>
2024-04-26 12:41:24 +0530

    Configure Things for Deployment

--- src/Bird.tsx
+++ src/Bird.tsx
@@ -1,7 +1,8 @@
+import bird from './assets/bird.png';
 function BirdImage() {
     return (
         <div className="profileImage">
-            <img src='./src/assets/bird.png' alt="Bird" />
+            <img src={bird} alt="Bird" />
         </div>
     );
 }
