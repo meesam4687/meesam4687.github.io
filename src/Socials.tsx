bfbc1c3544e12424c28c20ee38f0b0f71a79d842
meesam4687 <meesam4687@gmail.com>
2024-05-04 20:30:50 +0530

    feat: Change the way links are opened

--- src/Socials.tsx
+++ src/Socials.tsx
@@ -3,9 +3,9 @@ import discord from './assets/discord.svg';
 import twitter from './assets/twitter.svg';
 
 function Socials() {
-    let githubUrl = () => { window.location.href = "https://github.com/meesam4687" }
-    let discordUrl = () => { window.location.href = "https://discord.com/users/809702164724449290" }
-    let twitterUrl = () => { window.location.href = "https://twitter.com/msm4687" }
+    let githubUrl = () => { window.open("https://github.com/meesam4687") }
+    let discordUrl = () => { window.open("https://discord.com/users/809702164724449290") }
+    let twitterUrl = () => { window.open("https://twitter.com/msm4687") }
     return (
         <div className="socials">
             <img onClick={githubUrl} className="icon" src={github} />
--- src/Text.tsx
+++ src/Text.tsx
@@ -1,5 +1,5 @@
 function Text() {
-    let openRepos = () => { window.location.href = "https://github.com/meesam4687?tab=repositories" }
+    let openRepos = () => { window.open("https://github.com/meesam4687?tab=repositories") }
     return (
         <div className="aboutText"><p className="txt1">Hello&nbsp;<img className="waveGif" src="https://em-content.zobj.net/source/noto-emoji-animations/344/waving-hand_light-skin-tone_1f44b-1f3fb_1f3fb.gif"></img></p>
             <p className="txt1">I am Meesam</p>
