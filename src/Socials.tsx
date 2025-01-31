81abbddb97a1f568a24cbe5648cac9ff050230f9
Meesam <meesam4687@gmail.com>
2025-01-31 19:03:30 +0530

    Add a LinkedIn icon in the socials tab
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/meesam4687/meesam4687.github.io?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/Socials.tsx
+++ src/Socials.tsx
@@ -1,16 +1,19 @@
 import github from './assets/github.svg';
 import discord from './assets/discord.svg';
 import twitter from './assets/twitter.svg';
+import linkedin from './assets/linkedin.svg';
 
 function Socials() {
     let githubUrl = () => { window.open("https://github.com/meesam4687") }
     let discordUrl = () => { window.open("https://discord.com/users/809702164724449290") }
     let twitterUrl = () => { window.open("https://twitter.com/msm4687") }
+    let linkedinUrl = () => { window.open("https://www.linkedin.com/in/meesam4687/") }
     return (
         <div className="socials">
             <img onClick={githubUrl} className="icon" src={github} />
             <img onClick={discordUrl} className="icon" src={discord} />
             <img onClick={twitterUrl} className="icon" src={twitter} />
+            <img onClick={linkedinUrl} className="icon" src={linkedin} />
         </div>
     );
 }
